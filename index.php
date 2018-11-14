7e1713b4af063267ef87556d0494b624c6c8093d
Aluno <aluno@edu.senac.br>
2018-11-14 21:43:13 -0200

    Adicionando php no index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>
+		<?php 
+			$nome = "Geovane";
+
+			echo $nome; //esse é um comentário
+			/*
+			esse é um comentário de 
+			várias linhas de texto e 
+			pode aumentar
+			*/
+			#outro tipo de comentário
+		 ?>
+	</h1>
+
+</body>
+</html>
