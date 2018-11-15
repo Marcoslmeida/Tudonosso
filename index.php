6bf567036a37eae35244c5d5a010cdf6bc7e03e1
Aluno <aluno@edu.senac.br>
2018-11-14 22:00:50 -0200

    Variaveis ensinando tipos

--- index.php
+++ index.php
@@ -7,8 +7,14 @@
 	<h1>
 		<?php 
 			$nome = "Geovane";
+			$nascimento = '06/09/1989';
+			$sobre_nome = 'Oliveira Tavares';
 
-			echo $nome; //esse é um comentário
+			/*echo $nome; //esse é um comentário
+			echo "<br />";
+			echo $nascimento;
+			echo "<br />";
+			echo $sobre_nome;*/
 			/*
 			esse é um comentário de 
 			várias linhas de texto e 
@@ -17,6 +23,9 @@
 			#outro tipo de comentário
 		 ?>
 	</h1>
+	<p><?php echo $nome; ?></p>
+	<p><?php echo $sobre_nome; ?></p>
+	<p><?php echo $nascimento; ?></p>
 
 </body>
 </html>
